53c5cd26ac919df868a31dd53fbca097f172818d
ParsaBN <parsa.babadi@yahoo.com>
2024-07-18 17:16:53 +1000

    can now store parts and mates to prevent repeated api calls when testing using -t

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 import os
 from airframe import create_airframe_config
+import pickle
 
 from api import Client
 from gazebo import create_gazebo_config, insert_gazebo_plugins
@@ -16,6 +17,7 @@ def main():
     parser.add_argument('document', type=str, help='Onshape document id')
     parser.add_argument('workspace', type=str, help='Onshape workspace id')
     parser.add_argument('element', type=str, help='Onshape element id')
+    parser.add_argument('-t', '--testing', action='store_true', help='Used previously fetched assembly, prevents unnecessary api calls')
     args = parser.parse_args()
 
     model_path = Path(args.models_path, args.name)
@@ -24,8 +26,27 @@ def main():
     meshes_path = Path(model_path, 'meshes/')
     meshes_path.mkdir(exist_ok=True)
 
-    client = Client('https://cad.onshape.com/api/v6', os.environ['ONSHAPE_ACCESS_KEY'], os.environ['ONSHAPE_SECRET_KEY'])
-    parts, mates = fetch_assembly(client, args.document, args.workspace, args.element, meshes_path)
+    parts_mates = Path(os.path.join(os.getcwd(), 'parts_mates'))
+    if not args.testing:
+        client = Client('https://cad.onshape.com/api/v6', os.environ['ONSHAPE_ACCESS_KEY'], os.environ['ONSHAPE_SECRET_KEY'])
+        parts, mates = fetch_assembly(client, args.document, args.workspace, args.element, meshes_path)
+
+        parts_mates.mkdir(exist_ok=True)
+
+        with open(os.path.join(parts_mates, 'parts.pickle'), 'wb') as handle:
+            pickle.dump(parts, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        with open(os.path.join(parts_mates, 'mates.pickle'), 'wb') as handle:
+            pickle.dump(mates, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    else:
+        print('Using stored assembly')
+        # NOTE: might want to add a check for whether the files or the directory exits or not
+        with open(os.path.join(parts_mates, 'parts.pickle'), 'rb') as handle:
+            parts = pickle.load(handle)
+
+        with open(os.path.join(parts_mates, 'mates.pickle'), 'rb') as handle:
+            mates = pickle.load(handle)
 
     sdf = create_sdf(args.name, parts, mates)
     insert_gazebo_plugins(sdf)
